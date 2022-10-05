a49520b81047171309d4478a78a2fde1a689516d
Touka19 <78088067+Touka19@users.noreply.github.com>
2022-10-05 16:00:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -58,12 +58,15 @@ setInterval(function() {
 		.then(function(data) {
 			var parsed = JSON.parse(data)
 			resp = parsed['title'] + ' ' + parsed['url'] + ' ' + parsed['test']
+                        pt = parsed['test'];
+                        getMIMEType(pt);
 			if (parsed['guid'] == uuid) {
 				// Do nothing!
+			} else if(Mimetypes.includes(pt)) {
+                                console.log('mimetypes test');
 			} else {
 				console.log('Found something new!');
 				caption = parsed['title'];
-				pt = parsed['test'];
 				bot.sendDocument(oid, pt, {
 					caption
 				});
